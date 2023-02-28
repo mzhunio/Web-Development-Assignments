d34591dee4f89f69d29cf50b76dc4ea811258ffe
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-02-27 21:31:13 -0500

    added NavBar.vue

--- fitness-app/src/views/HomeView.vue
+++ fitness-app/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+<main>
+    <h1 class="title">
+    This is our home page
+  </h1>
   </main>
 </template>
