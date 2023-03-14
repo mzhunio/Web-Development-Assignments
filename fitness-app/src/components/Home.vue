c1452bbe0f0ab6871dcba0d99dd9512e22988498
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-13 20:08:27 -0400

    add home page

--- fitness-app/src/components/Home.vue
+++ fitness-app/src/components/Home.vue
@@ -1,13 +1,87 @@
 <script setup lang="ts">
-
+let count = 0;
 </script>
 
 <template>
-    Home Page
-    Tables of how many hours I workout
-  
+  <div class="container">
+    <div class="column">
+        <div class="box">
+        <h2 class="tittle">Today</h2>
+        <div class="columns is-mobile is-centered is-multiline">
+            <div class="column is-half">
+              <h3 class="value">1.5</h3>
+              <caption class="caption">
+                Distance
+              </caption>
+            </div>
+            <div class="column is-half">
+              <h3 class="value">3 Hours</h3>
+              <caption class="caption">
+                Duration
+              </caption>
+            </div>
+            <div class="column is-half">
+              <h3 class="value">1000</h3>
+              <caption class="caption">
+                Calories
+              </caption>
+            </div>
+            <div class="column is-half">
+              <h3 class="value">1.5</h3>
+              <caption class="caption">
+                Ave Page
+              </caption>
+            </div>
+        </div>
+      </div>
+
+      <div class="box">
+        <h2 class="tittle">Last Week</h2>
+        <div class="columns is-mobile is-centered is-multiline">
+            <div class="column is-half">
+              <h3 class="value">1.5</h3>
+              <caption class="caption">
+                Distance
+              </caption>
+            </div>
+            <div class="column is-half">
+              <h3 class="value">3 Hours</h3>
+              <caption class="caption">
+                Duration
+              </caption>
+            </div>
+            <div class="column is-half">
+              <h3 class="value">1000</h3>
+              <caption class="caption">
+                Calories
+              </caption>
+            </div>
+            <div class="column is-half">
+              <h3 class="value">1.5</h3>
+              <caption class="caption">
+                Ave Page
+              </caption>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  text-align: center;
+  margin-top: 60px;
+  height: 100%;
+}
+
+.columns {
+  display: block;
+  padding: 2;
+  padding: 0.75%;
+}
+.columns:not(.is-desktop) {
+  display: flex;
+}
 
 </style>
