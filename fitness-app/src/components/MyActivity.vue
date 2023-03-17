9afc4b5a8f61e5048ae379f5f47cf03db22d7c49
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-17 17:44:33 -0400

    wip

--- fitness-app/src/components/MyActivity.vue
+++ fitness-app/src/components/MyActivity.vue
@@ -1,33 +1,71 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+function addWorkout() {
+  console.log(true);
+}
+
+function onSave() {
+
+ }
+</script>
+
+
 
 <template>
   <div class="myActivity">
     <div class="container">
       <div class="columns is-justify-content-center">
-        <div
-          class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
-        >
-          <button class="button is-warning is-focused is-fullwidth">
-            Create Workout
-          </button>
-          <form>
-            <div class="modal">
-                <div class="block">
-                  This text is within a <strong>block</strong>.
+        <div class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen">
+         
+        <button type="button" class="button is-warning is-focused is-fullwidth" data-toggle="modal" data-target="add-workout">Create Workout</button>
+
+          <div id="add-workout" class="modal" role="dialog">
+            <div class="modal-dialog">
+
+            <div class="modal-card">
+              <header class="modal-card-head">
+                <p class="modal-card-title">Create Workout</p>
+                <button class="close" type="button" data-dismiss="modal">&times;</button>
+              </header>
+              <section class="modal-card-body"></section>
+              <form id="contact-form">
+  
+              <div class="field">
+                <label class="label">Workout Name</label>
+                <div class="control">
+                  <input class="input" type="text" placeholder="Text input" />
                 </div>
-                <div class="block">
-                  This text is within a <strong>second block</strong>. Lorem
-                  ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                  efficitur sit amet massa fringilla egestas. Nullam condimentum
-                  luctus turpis.
+              </div>
+
+              <div class="field">
+                <label class="label">Sets</label>
+                <div class="control">
+                  <input class="input" type="text" placeholder="Text input" />
                 </div>
-                <div class="block">
-                  This text is within a <strong>third block</strong>. This block
-                  has no margin at the bottom.
+              </div>
+
+              <div class="field">
+                <label class="label">Repetitions</label>
+                <div class="control">
+                  <input class="input" type="text" placeholder="Text input" />
                 </div>
+              </div>
+
+              <div class="field">
+                <label class="label">Weight</label>
+                <div class="control">
+                  <input class="input" type="text" placeholder="Text input" />
+                </div>
+              </div>
+            </form>
+              <footer class="modal-card-foot">
+                <button class="button is-success" id="closeModal" @click="onSave()">Save changes</button>
+                <button class="button">Cancel</button>
+              </footer>
             </div>
-          </form>
+          </div>
         </div>
+      </div>
       </div>
     </div>
   </div>
