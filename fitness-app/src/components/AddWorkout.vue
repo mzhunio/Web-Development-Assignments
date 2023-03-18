747fd8fbbf991b0465eea2dc6dbaf6f3561b880d
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-18 00:27:53 -0400

    workout format

--- fitness-app/src/components/AddWorkout.vue
+++ fitness-app/src/components/AddWorkout.vue
@@ -1,48 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="add-workout" class="modal">
-    <div class="modal-background"></div>
-    <div class="modal-card">
-      <header class="modal-card-head">
-        <p class="modal-card-title">Create Workout</p>
-        <button class="delete" aria-label="close"></button>
-      </header>
-      <section class="modal-card-body"></section>
-      <div class="field">
-        <label class="label">Workout Name</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="Text input" />
-        </div>
-      </div>
-
-      <div class="field">
-        <label class="label">Sets</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="Text input" />
-        </div>
-      </div>
-
-      <div class="field">
-        <label class="label">Repetitions</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="Text input" />
-        </div>
-      </div>
-
-      <div class="field">
-        <label class="label">Weight</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="Text input" />
-        </div>
-      </div>
-
-      <footer class="modal-card-foot">
-        <button class="button is-success">Save changes</button>
-        <button class="button">Cancel</button>
-      </footer>
-    </div>
-  </div>
+ 
+ 
 </template>
 
 <style scoped></style>
--- fitness-app/src/components/MyActivity.vue
+++ fitness-app/src/components/MyActivity.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { workouts } from "@/state/auth.state";
+import { authState } from "../state/auth.state";
 
+const session = authState.username;
 const shouldShowModal = ref(false);
 const workoutTypes = ref(["weights", "outdoor"]);
 const workoutType = ref("");
@@ -65,7 +67,7 @@ function onSaveChangesClicked() {
                   <p class="title">Create Workout</p>
 
                   <div class="field">
-                    <label class="label">Workout Name</label>
+                    <label class="label">Workout Type</label>
                     <div class="control">
                       <input
                         class="input"
@@ -150,6 +152,57 @@ function onSaveChangesClicked() {
           </div>
 
           <!-- list of workouts -->
+
+          <div class="box">
+            <article class="media">
+              <div class="media-left">
+                <figure class="image is-64x64">
+                  <img
+                    src="https://bulma.io/images/placeholders/128x128.png"
+                    alt="Image"
+                  />
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>Michelle Zhunio</strong> <small> {{ session }}</small>
+                    <small>31m</small>
+                    <br />
+                    <div v-for="workout in workouts">
+
+                  
+                      <div> 
+                      Workout Name = {{ workout.workoutName }}, Repetitions =
+                      {{ workout.repetitions }}, Sets = {{ workout.sets }}
+                      </div>
+                    </div>
+                  </p>
+                </div>
+                <nav class="level is-mobile">
+                  <div class="level-left">
+                    <a class="level-item" aria-label="reply">
+                      <span class="icon is-small">
+                        <i class="fas fa-reply" aria-hidden="true"></i>
+                      </span>
+                    </a>
+                    <a class="level-item" aria-label="retweet">
+                      <span class="icon is-small">
+                        <i class="fas fa-retweet" aria-hidden="true"></i>
+                      </span>
+                    </a>
+                    <a class="level-item" aria-label="like">
+                      <span class="icon is-small">
+                        <i class="fas fa-heart" aria-hidden="true"></i>
+                      </span>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+            </article>
+          </div>
+
+
           <div v-for="workout in workouts">
             <div>
               Workout Name = {{ workout.workoutName }}, Repetitions =
