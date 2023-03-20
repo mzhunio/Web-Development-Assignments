ec2414411df14c74ecaaf06df0cf06b0f4749065
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-19 23:27:05 -0400

    something

--- fitness-app/src/router/index.ts
+++ fitness-app/src/router/index.ts
@@ -1,73 +1,78 @@
 
-import User from '@/components/User.vue'
-import Home from '@/components/Home.vue'
-import Search from '@/components/Search.vue'
-import FriendsActivity from '@/components/FriendsActivity.vue'
-import FriendsActivityWorkout from '@/components/FriendsActivityWorkout.vue'
-import MyActivity from '@/components/MyActivity.vue'
-import Login from '@/components/Login.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import Register from '@/components/Register.vue'
-import Modal from '@/components/Modal.vue'
+import Activity from "@/components/Activity.vue";
+import FriendsActivity from "@/components/FriendsActivity.vue";
+import FriendsActivityWorkout from "@/components/FriendsActivityWorkout.vue";
+import Home from "@/components/Home.vue";
+import Login from "@/components/Login.vue";
+import Modal from "@/components/Modal.vue";
+import MyActivity from "@/components/MyActivity.vue";
+import Register from "@/components/Register.vue";
+import Search from "@/components/Search.vue";
+import User from "@/components/User.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/myActivity',
-      name: 'myActivity',
-      component: MyActivity
+      path: "/myActivity",
+      name: "myActivity",
+      component: MyActivity,
     },
     {
-      path: '/friendsActivity',
-      name: 'friendsActivity',
-      component: FriendsActivity
+      path: "/friendsActivity",
+      name: "friendsActivity",
+      component: FriendsActivity,
     },
     {
-      path: '/friendsActivity/workout/:username',
-      name: 'Friends ACtivity Workout',
-      component: FriendsActivityWorkout
+      path: "/friendsActivity/workout/:username",
+      name: "Friends ACtivity Workout",
+      component: FriendsActivityWorkout,
     },
     {
-      path: '/search',
-      name: 'search',
-      component: Search
+      path: "/search",
+      name: "search",
+      component: Search,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../components/About.vue')
+      component: () => import("../components/About.vue"),
     },
     {
-      path: '/user',
-      name: 'user',
-      component: User
+      path: "/user",
+      name: "user",
+      component: User,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: Login
+      path: "/login",
+      name: "login",
+      component: Login,
     },
     {
-      path: '/register',
-      name: 'register',
-      component: Register
+      path: "/register",
+      name: "register",
+      component: Register,
     },
     {
-      path: '/modal',
-      name: 'modal',
-      component: Modal
-    }
-
-  ]
-})
+      path: "/modal",
+      name: "modal",
+      component: Modal,
+    },
+    {
+      path: "/activity",
+      name: "Activity",
+      component: Activity,
+    },
+  ],
+});
 
 export default router
