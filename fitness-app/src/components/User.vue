970dd74b72473c827cc1da5e9b13670e5248334a
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-21 01:54:49 -0400

    users table

--- fitness-app/src/components/User.vue
+++ fitness-app/src/components/User.vue
@@ -1,18 +1,84 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import router from "@/router";
+
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+const user = ref<User>({
+  id: 0,
+  username: "mzhunio",
+  email: "mzhunio@a.com",
+  isAdmin: true,
+});
+
+// const usersInformation = [
+//   { id: 0, username: "mzhunio", email: "mzhunio@a.com", isAdmin: true },
+//   { id: 1, username: "rzhunio", email: "rzhunio@a.com", isAdmin: false },
+//   { id: 3, username: "kzhunio", email: "kzhunio@a.com", isAdmin: false },
+// ];
+
+const mzhunioUser = [
+  {
+    id: 0,
+    username: "mzhunio",
+    email: "mzhunio@a.com",
+    isAdmin: true,
+  },
+];
+
+const rzhunioUser = [
+  {
+    id: 1,
+    username: "rzhunio",
+    email: "rzhunio@a.com",
+    isAdmin: false,
+  },
+];
+
+const kzhunioUser = [
+  {
+    id: 2,
+    username: "kzhunio",
+    email: "kzhunio@a.com",
+    isAdmin: false,
+  },
+];
+
+const usersMap = {
+  mzhunio: mzhunioUser,
+  rzhunio: rzhunioUser,
+  kzhunio: kzhunioUser,
+};
+
+const usernames = ref<User[]>(mzhunioUser);
+
+const username = router.currentRoute.value.params
+  .username as keyof typeof usersMap;
+const usersInfo = usersMap[username];
+
+if (usersInfo) {
+  usernames.value = usersInfo;
+} else {
+  console.error("Sorry, I could not found provided user.");
+}
+</script>
 
 <template>
   <div class="user">
     <div class="container is-max-desktop">
       <div class="buttons mt-5">
-        <button
-          class="button fa-light fa-plus is-info is-light"
-        >
+        <button class="button fa-light fa-plus is-info is-light">
           Add User
         </button>
       </div>
       <div class="column is-three-fifths">
         <div class="box">
-          <table class="table">
+          <table class="table" v-for="username in usernames">
             <thead>
               <tr>
                 <th><abbr title="id">Id</abbr></th>
@@ -25,14 +91,26 @@
             <tfoot></tfoot>
             <tbody>
               <tr>
-                <th>1</th>
-                <th><abbr title="Position">mzhunio</abbr></th>
-                <th><abbr title="Played">mzhunio@a.com</abbr></th>
-                <th><abbr title="Played">true</abbr></th>
+                <th>
+                  <abbr title="id">{{ username.id }}</abbr>
+                </th>
+                <th>
+                  <abbr title="username">{{ username.username }}</abbr>
+                </th>
+                <th>
+                  <abbr title="email">{{ username.email }}</abbr>
+                </th>
+                <th>
+                  <abbr title="isAdmin">{{ username.isAdmin }}</abbr>
+                </th>
                 <th>
                   <abbr title="Played">
-                    <button class="button"><i class="fas fa-edit"></i></button>
-                    <button class="button"><i class="fas fa-trash"></i></button>
+                    <button class="button">
+                      <i class="fas fa-edit"></i>
+                    </button>
+                    <button class="button">
+                      <i class="fas fa-trash"></i>
+                    </button>
                   </abbr>
                 </th>
               </tr>
@@ -45,14 +123,12 @@
 </template>
 
 <style scoped>
-
-.user {
-display: block;
-margin-left: auto;
-margin-right:5rem;
+.container {
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
 }
 .buttons {
-margin-left: 1em;
+  margin-left: 1em;
 }
-
 </style>
