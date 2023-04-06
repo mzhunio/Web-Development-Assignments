82179b052bf795ccf202e450509247d3bd2efb3e
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-04-05 23:03:24 -0400

    Add User API for

--- server/index.js
+++ server/index.js
@@ -1,36 +1,158 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql2/promise");
 
-const app = express();
+// 1. async await
+async function startServer() {
+  const app = express();
 
-app.use(bodyParser.json());
-app.use(cors());
+  app.use(bodyParser.json());
+  app.use(cors());
 
-let workouts = [];
+  const connection = await mysql.createConnection({
+    socketPath: "/tmp/mysql.sock",
+    user: "root",
+    password: "",
+    database: "fitness",
+  });
 
-app.get("/api/v1/", (req, res) => {
-  res.send("Hello World From Express");
-});
+  app.get("/", (req, res) => {
+    res.send("Welcome to the Fitness API");
+  });
 
-app.get("/api/v1/workouts", (req, res) => {
-  res.json(workouts);
-});
+  async function getUser(id) {
+    const [rows] = await connection.execute("SELECT * FROM User WHERE id = ?", [
+      id,
+    ]);
+    return rows[0];
+  }
 
-app.post("/api/v1/workouts", (req, res) => {
-  const { workoutName } = req.body;
-  const newExercise = {
-    id: workouts.length + 1,
-    workoutName: workoutName,
-  };
+  // TODO: Add this to POST /user
+  async function createUser(username, email, password, isAdmin) {
+    const [result] = await connection.execute(
+      `INSERT INTO User (username, email, password, isAdmin) VALUES (?, ?, ?, ?);`,
+      [username, email, password, isAdmin]
+    );
 
-  workouts.push(newExercise);
-  res.json(newExercise);
-});
+    const [rows] = await connection.execute(`SELECT * FROM User WHERE id = ?`, [
+      result.insertId,
+    ]);
 
-const port = 3000;
+    return rows[0];
+  }
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  // TODO: Add this to UPDATE /user/:id
+  async function updateUser(id, password, isAdmin) {
+    await connection.execute(
+      "UPDATE User SET password = ?, isAdmin = ? WHERE id = ?;",
+      [password, isAdmin, id]
+    );
 
+    const [rows] = await connection.execute("SELECT * FROM User WHERE id = ?", [
+      id,
+    ]);
+
+    return rows[0];
+  }
+
+  // TODO: Add this to DELETE /user/:id
+  async function deleteUser(id) {
+    // TODO: Add functionality to delete user from database
+  }
+
+  app.get("/user/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      // TODO: See how this it is?
+      const user = await getUser(id);
+      res.send(user);
+    } catch (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+  });
+
+  app.get("/user", async (req, res) => {
+    try {
+      const [rows] = await connection.execute("SELECT * FROM User;");
+      res.send(rows);
+    } catch (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+  });
+
+  app.post("/user", async (req, res) => {
+    const { username, email, password, isAdmin } = req.body;
+
+    const [result] = await connection.execute(
+      `INSERT INTO User (username, email, password, isAdmin) VALUES (?, ?, ?, ?);`,
+      [username, email, password, isAdmin]
+    );
+
+    const [rows] = await connection.execute(`SELECT * FROM User WHERE id = ?`, [
+      result.insertId,
+    ]);
+    const [user] = rows;
+
+    res.send(user);
+  });
+
+  app.put("/user/:id", async (req, res) => {
+    const { id } = req.params;
+    // 2. object destructuring
+    const { password, isAdmin } = req.body;
+
+    try {
+      await connection.execute(
+        "UPDATE User SET password = ?, isAdmin = ? WHERE id = ?;",
+        [password, isAdmin, id]
+      );
+
+      const [rows] = await connection.execute(
+        "SELECT * FROM User WHERE id = ?",
+        [id]
+      );
+      // 3. object destructuring
+      const [user] = rows;
+
+      res.send(user);
+    } catch (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+  });
+
+  app.delete("/user/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const [rows] = await connection.execute("SELECT * FROM User WHERE id = ?", [
+      id,
+    ]);
+    const [user] = rows;
+
+    if (user) {
+      await connection.execute("DELETE FROM User WHERE id = ?", [id]);
+
+      res.send(user);
+    } else {
+      res.status(400).send({
+        message: `Cannot delete user with id ${id}`,
+      });
+    }
+
+    res.send(result);
+  });
+
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`✅ Server running on http://localhost:${port}`);
+  });
+}
+
+startServer();
