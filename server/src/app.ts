f96822892a23e449d6657df0af266df20fd826fd
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-04-08 01:03:10 -0400

    make it better

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,22 +3,23 @@ import cors from "cors";
 import express from "express";
 import { userRouter } from "./user/user.router";
 
-async function startServer() {
-  const app = express();
+const app = express();
 
-  app.use(bodyParser.json());
-  app.use(cors());
+app.use(bodyParser.json());
+app.use(cors());
 
-  app.get("/", (req, res) => {
-    res.send("Welcome to the Fitness API");
-  });
+app.get("/", (req, res) => {
+  res.send("Welcome to the Fitness API");
+});
 
-  app.use(userRouter);
+app.use(userRouter);
 
-  const port = 3000;
-  app.listen(port, () =>
-    console.log(`✅ Server running on http://localhost:${port}`)
-  );
-}
+const port = 3000;
+const server = app.listen(port, () =>
+  console.log(`✅ Server running on http://localhost:${port}`)
+);
 
-startServer();
+export const closeServer = () =>
+  new Promise((resolve) => server.close(resolve));
+
+export { app };
