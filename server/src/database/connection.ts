687431e868a8a3a3f17ccc2d6aebe782905fc0e1
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-04-08 09:52:31 -0400

    Add password to sql client

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -3,7 +3,7 @@ import mysql, { Pool, PoolOptions } from "mysql2/promise";
 const poolOptions: PoolOptions = {
   socketPath: "/tmp/mysql.sock",
   user: "root",
-  password: "",
+  password: "password",
   database: "fitness",
   connectionLimit: 10,
 };
