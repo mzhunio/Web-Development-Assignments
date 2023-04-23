555d382ce864b1e29e80eae159c8800d759428f5
Michi <63206155+mzhunio@users.noreply.github.com>
2023-04-23 15:19:42 -0400

    fix folder

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -3,87 +3,87 @@ import { CreateUserModel, UpdateUserModel } from "./user.model";
 import { UserService } from "./user.service";
 
 export class UserController {
-  private userService = new UserService();
+  // private userService = new UserService();
 
-  async getAllUsers(req: Request, res: Response) {
-    try {
-      const users = await this.userService.getUsers();
-      res.send(users);
-    } catch (err: any) {
-      const message = err.message ?? `Cannot fetch all users`;
-      res.status(400).send({ message });
-    }
-  }
+  // async getAllUsers(req: Request, res: Response) {
+  //   try {
+  //     const users = await this.userService.getUsers();
+  //     res.send(users);
+  //   } catch (err: any) {
+  //     const message = err.message ?? `Cannot fetch all users`;
+  //     res.status(400).send({ message });
+  //   }
+  // }
 
-  async getUser(req: Request, res: Response) {
-    const { id } = req.params;
+  // async getUser(req: Request, res: Response) {
+  //   const { id } = req.params;
 
-    try {
-      const user = await this.userService.getUser(parseInt(id, 10));
-      res.send(user);
-    } catch (err: any) {
-      const message = err.message ?? `Cannot get user with id ${id}`;
-      res.status(400).send({ message });
-    }
-  }
+  //   try {
+  //     const user = await this.userService.getUser(parseInt(id, 10));
+  //     res.send(user);
+  //   } catch (err: any) {
+  //     const message = err.message ?? `Cannot get user with id ${id}`;
+  //     res.status(400).send({ message });
+  //   }
+  // }
 
-  async getUserWorkouts(req: Request, res: Response) {
-    const { id } = req.params;
+  // async getUserWorkouts(req: Request, res: Response) {
+  //   const { id } = req.params;
 
-    try {
-      const userWorkouts = await this.userService.getUserWorkouts(
-        parseInt(id, 10)
-      );
+  //   try {
+  //     const userWorkouts = await this.userService.getUserWorkouts(
+  //       parseInt(id, 10)
+  //     );
 
-      res.send(userWorkouts);
-    } catch (err: any) {
-      const message = err.message ?? `Cannot get user workouts with id ${id}`;
-      res.status(400).send({ message });
-    }
-  }
+  //     res.send(userWorkouts);
+  //   } catch (err: any) {
+  //     const message = err.message ?? `Cannot get user workouts with id ${id}`;
+  //     res.status(400).send({ message });
+  //   }
+  // }
 
-  async createUser(req: Request, res: Response) {
-    const { username, email, password, isAdmin } = req.body as CreateUserModel;
+  // async createUser(req: Request, res: Response) {
+  //   const { username, email, password, isAdmin } = req.body as CreateUserModel;
 
-    try {
-      const user = await this.userService.createUser({
-        username,
-        email,
-        password,
-        isAdmin,
-      });
-      res.send(user);
-    } catch (err: any) {
-      const message = err.message ?? `Cannot create user`;
-      res.status(400).send({ message });
-    }
-  }
+  //   try {
+  //     const user = await this.userService.createUser({
+  //       username,
+  //       email,
+  //       password,
+  //       isAdmin,
+  //     });
+  //     res.send(user);
+  //   } catch (err: any) {
+  //     const message = err.message ?? `Cannot create user`;
+  //     res.status(400).send({ message });
+  //   }
+  // }
 
-  async updateUser(req: Request, res: Response) {
-    const { id } = req.params;
-    const changes = req.body as UpdateUserModel;
+  // async updateUser(req: Request, res: Response) {
+  //   const { id } = req.params;
+  //   const changes = req.body as UpdateUserModel;
 
-    try {
-      const user = await this.userService.updateUser(parseInt(id, 10), changes);
-      res.send(user);
-    } catch (err: any) {
-      const message = err.message ?? `Cannot update user with id ${id}`;
+  //   try {
+  //     const user = await this.userService.updateUser(parseInt(id, 10), changes);
+  //     res.send(user);
+  //   } catch (err: any) {
+  //     const message = err.message ?? `Cannot update user with id ${id}`;
 
-      res.status(400).send({ message });
-    }
-  }
+  //     res.status(400).send({ message });
+  //   }
+  // }
 
-  async deleteUser(req: Request, res: Response) {
-    const { id } = req.params;
-    await this.userService.getUser(parseInt(id, 10));
+  // async deleteUser(req: Request, res: Response) {
+  //   const { id } = req.params;
+  //   await this.userService.getUser(parseInt(id, 10));
 
-    try {
-      const user = await this.userService.deleteUser(parseInt(id, 10));
-      res.send(user);
-    } catch (err: any) {
-      const message = err.message ?? `Cannot delete user with id ${id}`;
+  //   try {
+  //     const user = await this.userService.deleteUser(parseInt(id, 10));
+  //     res.send(user);
+  //   } catch (err: any) {
+  //     const message = err.message ?? `Cannot delete user with id ${id}`;
 
-      res.status(400).send({ message });
-    }
-  }
+  //     res.status(400).send({ message });
+  //   }
+  // }
 }
--- server/src/user/user.router.ts
+++ server/src/user/user.router.ts
@@ -4,14 +4,14 @@ import { UserController } from "./user.controller";
 const userRouter = Router();
 const userController = new UserController();
 
-userRouter.get("/user", userController.getAllUsers.bind(userController));
-userRouter.get("/user/:id", userController.getUser.bind(userController));
-userRouter.get(
-  "/user/:id/workouts",
-  userController.getUserWorkouts.bind(userController)
-);
-userRouter.post("/user", userController.createUser.bind(userController));
-userRouter.patch("/user/:id", userController.updateUser.bind(userController));
-userRouter.delete("/user/:id", userController.deleteUser.bind(userController));
+// userRouter.get("/user", userController.getAllUsers.bind(userController));
+// userRouter.get("/user/:id", userController.getUser.bind(userController));
+// userRouter.get(
+//   "/user/:id/workouts",
+//   userController.getUserWorkouts.bind(userController)
+// );
+// userRouter.post("/user", userController.createUser.bind(userController));
+// userRouter.patch("/user/:id", userController.updateUser.bind(userController));
+// userRouter.delete("/user/:id", userController.deleteUser.bind(userController));
 
 export { userRouter };
